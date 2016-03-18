d198cb127bb27ec0131b3e4de3dc24bc5dfd4a22
kyoungchin <kcseo.acm@gmail.com>
2016-03-18 15:40:30 +0900

    add model for Question/Answer

---
+++ src/main/java/next/model/Answer.java
@@ -0,0 +1,12 @@
+package next.model;
+
+public class Answer {
+	private String answerId;
+	private String writer;
+	private String contents;
+	private String createDate;
+	private String questionId;
+	
+	
+	
+}
---
+++ src/main/java/next/model/Question.java
@@ -0,0 +1,13 @@
+package next.model;
+
+public class Question {
+	private String questionId;
+	private String writer;
+	private String title;
+	private String contents;
+	private String createDate;
+	private String countOfAnswer;
+	
+
+	
+}
