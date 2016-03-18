cd39c0cba87e3c7c611c688471c6839718b8b88d
kyoungchin <kcseo.acm@gmail.com>
2016-03-18 14:13:45 +0900

    fix HomeController.java

--- src/main/java/next/controller/HomeController.java
+++ src/main/java/next/controller/HomeController.java
@@ -10,10 +10,9 @@ import next.dao.UserDao;
 public class HomeController implements Controller {
 	@Override
 	public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-		//req.setAttribute("users", DataBase.findAll());
 
-		//UserDao userDao = new UserDao();
-		//req.setAttribute("users",userDao.findAll());
+		UserDao userDao = new UserDao();
+		req.setAttribute("users",userDao.findAll());
 		
 		
 		return "index.jsp";
