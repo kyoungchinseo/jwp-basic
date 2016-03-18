b01b2e9d7384f0c949ce98cbebcf78b5e6247e7b
kyoungchin <kcseo.acm@gmail.com>
2016-03-19 05:02:39 +0900

    test

--- src/main/java/core/mvc/RequestMapping.java
+++ src/main/java/core/mvc/RequestMapping.java
@@ -34,9 +34,9 @@ public class RequestMapping {
 	    mappings.put("/users/create", new CreateUserController());
 	    mappings.put("/users/updateForm", new UpdateFormUserController());
 	    mappings.put("/users/update", new UpdateUserController());
-	    //mappings.put("/qna/form", new QnaFormController());
-	    //mappings.put("/qna/update", new UpdateQuestionController());
-	    //mappings.put("/qna/show", new ShowQnAController());
+	    mappings.put("/qna/form", new QnaFormController());
+	    mappings.put("/qna/update", new UpdateQuestionController());
+	    mappings.put("/qna/show", new ShowQnAController());
 
 		logger.info("Initialized Request Mapping!");
 	}
