6290e1ac9053209be85424e399d67b572ece0a8f
kyoungchin <kcseo.acm@gmail.com>
2016-03-18 16:26:35 +0900

    generate methods of Answer / Question model

--- src/main/java/next/model/Answer.java
+++ src/main/java/next/model/Answer.java
@@ -1,12 +1,91 @@
 package next.model;
 
+import java.sql.Timestamp;
+
 public class Answer {
-	private String answerId;
+	private long answerId;
 	private String writer;
 	private String contents;
-	private String createDate;
-	private String questionId;
+	private Timestamp createDate;
+	private long questionId;
 	
+	public Answer(long answerId, String writer, String contents,
+			Timestamp createDate, long questionId) {
+		this.answerId = answerId;
+		this.writer = writer;
+		this.contents = contents;
+		this.createDate = createDate;
+		this.questionId = questionId;
+	}
+
+	public long getAnswerId() {
+		return answerId;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public Timestamp getCreateDate() {
+		return createDate;
+	}
+
+	public long getQuestionId() {
+		return questionId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (answerId ^ (answerId >>> 32));
+		result = prime * result + ((contents == null) ? 0 : contents.hashCode());
+		result = prime * result + ((createDate == null) ? 0 : createDate.hashCode());
+		result = prime * result + (int) (questionId ^ (questionId >>> 32));
+		result = prime * result + ((writer == null) ? 0 : writer.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Answer other = (Answer) obj;
+		if (answerId != other.answerId)
+			return false;
+		if (contents == null) {
+			if (other.contents != null)
+				return false;
+		} else if (!contents.equals(other.contents))
+			return false;
+		if (createDate == null) {
+			if (other.createDate != null)
+				return false;
+		} else if (!createDate.equals(other.createDate))
+			return false;
+		if (questionId != other.questionId)
+			return false;
+		if (writer == null) {
+			if (other.writer != null)
+				return false;
+		} else if (!writer.equals(other.writer))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Answer [answerId=" + answerId + ", writer=" + writer + ", contents=" + contents + ", createDate="
+				+ createDate + ", questionId=" + questionId + "]";
+	}
 	
 	
 }
--- src/main/java/next/model/Question.java
+++ src/main/java/next/model/Question.java
@@ -1,13 +1,103 @@
 package next.model;
 
+import java.sql.Timestamp;
+
 public class Question {
-	private String questionId;
+	private	long questionId;
 	private String writer;
 	private String title;
 	private String contents;
-	private String createDate;
-	private String countOfAnswer;
+	private Timestamp createDate;
+	private int countOfAnswer;
 	
+	public Question(long questionId, String writer, String title, String contents, Timestamp createDate,
+			int countOfAnswer) {
+		this.questionId = questionId;
+		this.writer = writer;
+		this.title = title;
+		this.contents = contents;
+		this.createDate = createDate;
+		this.countOfAnswer = countOfAnswer;
+	}
 
-	
+	public long getQuestionId() {
+		return questionId;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public Timestamp getCreateDate() {
+		return createDate;
+	}
+
+	public int getCountOfAnswer() {
+		return countOfAnswer;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((contents == null) ? 0 : contents.hashCode());
+		result = prime * result + countOfAnswer;
+		result = prime * result + ((createDate == null) ? 0 : createDate.hashCode());
+		result = prime * result + (int) (questionId ^ (questionId >>> 32));
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((writer == null) ? 0 : writer.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Question other = (Question) obj;
+		if (contents == null) {
+			if (other.contents != null)
+				return false;
+		} else if (!contents.equals(other.contents))
+			return false;
+		if (countOfAnswer != other.countOfAnswer)
+			return false;
+		if (createDate == null) {
+			if (other.createDate != null)
+				return false;
+		} else if (!createDate.equals(other.createDate))
+			return false;
+		if (questionId != other.questionId)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (writer == null) {
+			if (other.writer != null)
+				return false;
+		} else if (!writer.equals(other.writer))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Question [questionId=" + questionId + ", writer=" + writer + ", title=" + title + ", contents="
+				+ contents + ", createDate=" + createDate + ", countOfAnswer=" + countOfAnswer + "]";
+	}
+
+		
 }
