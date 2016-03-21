333c0464f18fd829da83e5f78084fba8f518d7a3
kyoungchin <kcseo.acm@gmail.com>
2016-03-21 12:31:51 +0900

    test setting

--- src/test/java/next/dao/QuestionDaoTest.java
+++ src/test/java/next/dao/QuestionDaoTest.java
@@ -18,28 +18,28 @@ public class QuestionDaoTest {
 
 	@Before
 	public void setUp() {
-//		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-//		populator.addScript(new ClassPathResource("jwp.sql"));
-//		DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+		populator.addScript(new ClassPathResource("jwp.sql"));
+		DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
 	}
 
 	@Test
 	public void test() throws Exception {
-//		Calendar calender = Calendar.getInstance();
-//		Timestamp  tsTimestamp = new Timestamp(calender.getTime().getTime());
-//		System.out.println(tsTimestamp.toString());
-//		Question expected = new Question(9,"hello","helo world","hello friend. welcome to question database.",
-//				tsTimestamp,0);
-//		System.out.println(expected.toString());
-//		QuestionDao questionDao = new QuestionDao();
-//		questionDao.insert(expected);
-//		System.out.println(expected.getQuestionId());
-//		
-//		Question returned = questionDao.fingByQuestionId(expected.getQuestionId());
-//		
-//		System.out.println(returned.toString());
-//		
-//		assertEquals(expected, returned);
+		Calendar calender = Calendar.getInstance();
+		Timestamp  tsTimestamp = new Timestamp(calender.getTime().getTime());
+		System.out.println(tsTimestamp.toString());
+		Question expected = new Question(9,"hello","helo world","hello friend. welcome to question database.",
+				tsTimestamp,0);
+		System.out.println(expected.toString());
+		QuestionDao questionDao = new QuestionDao();
+		questionDao.insert(expected);
+		System.out.println(expected.getQuestionId());
+		
+		Question returned = questionDao.fingByQuestionId(expected.getQuestionId());
+		
+		System.out.println(returned.toString());
+		
+		assertEquals(expected, returned);
 		
 		
 	}
