9b7dc67a6dc34fd07c4f9f210612a3e8c40a3a99
kyoungchin <kcseo.acm@gmail.com>
2016-03-24 14:25:51 +0900

    답변하기 추가 기능(ajax) 구현

---
+++ src/main/java/next/controller/qna/AddAnswerController.java
@@ -0,0 +1,43 @@
+package next.controller.qna;
+
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import core.mvc.Controller;
+import next.dao.AnswerDao;
+import next.model.Answer;
+
+public class AddAnswerController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Answer answer = new Answer(
+				req.getParameter("writer"),
+				req.getParameter("contents"),
+				Long.parseLong(req.getParameter("questionId")));
+		//System.out.println(answer.toString());
+		
+		AnswerDao answerDao = new AnswerDao();
+		try {
+			Answer savedAnswer = answerDao.insert(answer);
+			ObjectMapper mapper = new ObjectMapper();
+			resp.setContentType("application/json;charset=UTF-8");
+			PrintWriter out = resp.getWriter();
+			out.print(mapper.writeValueAsString(savedAnswer));
+		} catch(Exception e) {
+			System.out.println(e);
+		}	
+		
+		
+		return null;  // <=======================
+	}
+	
+
+}
--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -8,3 +8,30 @@ String.prototype.format = function() {
   });
 };
 
+$(".answerWrite input[type=submit]").click(addAnswer);
+
+function addAnswer(e) {
+	e.preventDefault();
+	var queryString = $("form[name=answer]").serialize();
+	
+	$.ajax({
+		type:'post',
+		url: '/api/qna/addanswer',
+		data: queryString,
+		dataType: 'json',
+		error: onError,
+		success: onSuccess,
+	});
+}
+
+function onError(xhr, status) {
+	alert("error");
+}
+
+function onSuccess(json, status) {
+	console.log(json);
+	var answerTemplate = $("#answerTemplate").html();
+	var template = answerTemplate.format(json.writer, new Date(json.createdDate), json.contents, json.answerId, json.answerId);
+	$(".qna-comment-slipp-articles").prepend(template);
+	
+}
