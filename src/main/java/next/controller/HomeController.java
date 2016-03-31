d362e537aade83ffc5f9f1fe4eea28e0697e5e1f
kyoungchin <kcseo.acm@gmail.com>
2016-03-31 10:38:36 +0900

    change homeController

--- src/main/java/next/controller/HomeController.java
+++ src/main/java/next/controller/HomeController.java
@@ -3,10 +3,17 @@ package next.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import core.mvc.Controller;
 import next.dao.QuestionDao;
 
 public class HomeController implements Controller {
+	
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
 	@Override
 	public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
 		QuestionDao questionDao = new QuestionDao();
